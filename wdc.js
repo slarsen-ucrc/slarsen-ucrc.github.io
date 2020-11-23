54e393bf1e6490c24f1c85762484714b93dfa117
slarsen-ucrc <63020921+slarsen-ucrc@users.noreply.github.com>
2020-11-23 15:09:22 -0700

    Create wdc.js
    Upload WDC javascript file

---
+++ wdc.js
@@ -0,0 +1,36 @@
+(function () {
+var myConnector = tableau.makeConnector();
+myConnector.getSchema = function (schemaCallback) {
+var cols = [
+{ id : "datetime", alias:"Datetime", dataType : tableau.dataTypeEnum.string },
+{ id : "storage", alias: "Storage",dataType : tableau.dataTypeEnum.string }
+];
+var tableInfo = {
+id : "columns",
+alias : "Lake Powell Reservoir Storage",
+columns : cols
+};
+schemaCallback([tableInfo]);
+};
+myConnector.getData = function(table, doneCallback) {
+$.getJSON("https://www.usbr.gov/uc/water/hydrodata/reservoir_data/919/json/17.json", function(resp) {
+var feat = resp;
+tableData = [];
+// Iterate over the JSON object
+for (var i = 0, len = feat.length; i < len; i++) {
+tableData.push({
+"datetime": feat[i]["Datetime"],
+"storage": feat[i]["Storage"]
+});
+}
+table.appendRows(tableData);
+doneCallback();
+});
+};
+tableau.registerConnector(myConnector);
+$(document).ready(function () {
+$("#submitButton").click(function () {
+tableau.connectionName = "columns";
+tableau.submit();
+});
+});})();
