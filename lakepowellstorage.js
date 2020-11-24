97e8892655a3a2a1a56a5ece3eb00bc80501d252
slarsen-ucrc <63020921+slarsen-ucrc@users.noreply.github.com>
2020-11-24 11:58:33 -0700

    Update lakepowellstorage.js

--- lakepowellstorage.js
+++ lakepowellstorage.js
@@ -5,7 +5,7 @@
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
-            id: "datetime8",
+            id: "datetime9",
             dataType: tableau.dataTypeEnum.string
         }
 		//, {
@@ -26,13 +26,13 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://www.usbr.gov/uc/water/hydrodata/reservoir_data/919/json/17.json", function(resp) {
-            var feat = resp.data,
+            var feat = resp,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-				"datetime8": feat[i].data[0]
+				"datetime9": feat[i]["data"]
             //    "storage": feat[i].data
                 });
             }
