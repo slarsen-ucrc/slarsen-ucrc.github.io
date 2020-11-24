0dbcf6c2b7b7a9d92b161eae037b0ec99bec99e1
slarsen-ucrc <63020921+slarsen-ucrc@users.noreply.github.com>
2020-11-23 20:02:28 -0700

    Update lakepowellstorage.js

--- lakepowellstorage.js
+++ lakepowellstorage.js
@@ -26,13 +26,13 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://www.usbr.gov/uc/water/hydrodata/reservoir_data/919/json/17.json", function(resp) {
-            var feat = resp,
+            var feat = resp.data,
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-				"datetime": feat[i].data[0].data
+				"datetime": feat[i][0]
             //    "storage": feat[i].data
                 });
             }
